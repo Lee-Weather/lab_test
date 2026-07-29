6f00b2df87a156389287e8bbab39d060103ecf37
cuizeyu <cuizeyu@weichai.com>
2026-07-29 09:51:23 +0800

    fix: make handle_deprecated_rsl_rl_cfg import optional (IsaacLab 2.2.0 compat)

--- robolab/scripts/rsl_rl/train.py
+++ robolab/scripts/rsl_rl/train.py
@@ -165,7 +165,12 @@ from isaaclab.envs import (
 from isaaclab.utils.dict import print_dict
 from isaaclab.utils.io import dump_yaml
 
-from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg as RslRlBaseRunnerCfg, RslRlVecEnvWrapper, handle_deprecated_rsl_rl_cfg
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg as RslRlBaseRunnerCfg, RslRlVecEnvWrapper
+try:
+    from isaaclab_rl.rsl_rl import handle_deprecated_rsl_rl_cfg
+except ImportError:
+    def handle_deprecated_rsl_rl_cfg(agent_cfg, *args, **kwargs):
+        return agent_cfg
 
 import isaaclab_tasks  # noqa: F401
 from isaaclab_tasks.utils.hydra import hydra_task_config
